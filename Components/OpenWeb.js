f63d285171d50eb2803c4c332618a585a40b6108
rishav-jha-mech <rishav.rj74@gmail.com>
2021-10-11 19:11:23 +0530

    ErrorLoading WebPage made better

--- Components/OpenWeb.js
+++ Components/OpenWeb.js
@@ -3,33 +3,24 @@ import {StyleSheet,Text,View} from 'react-native'
 import { WebView } from 'react-native-webview'
 import { useNavigation } from '@react-navigation/native';
 
-// import LoadingWebPage from '../Screens/LoadingWebPage';
 import ErrorLoadingWebPage from '../Screens/ErrorLoadingWebPage';
 
 
 export default function OpenWeb( { route } ) {
   const navigation = useNavigation();
-  // const [loading, setLoading] = useState(true) //Cant givve loading here because Loading ends only when the website is completely loaded which takes too much time in some websites, which is annoying, so for now we wont be using it.
   const [error, setError] = useState(false)
   const { urlToWebSite } = route.params;
 
-
-  // console.log(urlToWebSite)
-
     return (  
       <>
-        {/* {loading ? <LoadingWebPage /> : <></>} */}
-
         {error ? <ErrorLoadingWebPage /> 
           
           :
 
           <WebView
-            source = {{ uri: "https://google.com" }}
+            source = {{ uri: "httpas://google.com" }}
             style={styles.container}
             javaScriptEnabled={true}
-            // onLoadStart={() => {setLoading(true);console.log("Loading Started")} }
-            // onLoadEnd={() => {setLoading(false);console.log("Loading Ended")} }
             onError={() => setError(true)}
           />
         }
--- Screens/ErrorLoadingWebPage.js
+++ Screens/ErrorLoadingWebPage.js
@@ -1,23 +1,28 @@
 import React from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, ScrollView, Image } from 'react-native'
 
 import { useNavigation } from '@react-navigation/native';
 
+
 const ErrorLoadingWebPage = () => {
     const navigation = useNavigation();
-    
+    // https://www.monstersandcritics.com/wp-content/uploads/2016/03/secret-life-of-pets-happy-easter.jpg
     return (
-        <View style={styles.Container}>
-
+        <ScrollView style={styles.Container}>
+            <Image style={styles.ErrorImage} source={{uri : 'https://capestylemag.com/wp-content/uploads/2018/04/Snowball-from-The-Secret-Life-of-Pets.jpg'}} />
             <Text style={styles.bigText}>
                 Error Loading Page !
             </Text>
             <Text style={styles.smallText}>
                 The News which you are searching for may be moved or deleted
             </Text>
-            <Button title="Go To Home" onPress={() => {navigation.navigate('Home')} }/>
-
-        </View>
+            <Text style={styles.smallText}>
+                or try checking your internet connection.
+            </Text>
+            <Pressable style={styles.but} onPress={() => {navigation.navigate('Home')} }>
+            <Text style={styles.butText}> Go To Home </Text>
+            </Pressable>
+        </ScrollView>
     )
 }
 
@@ -26,19 +31,38 @@ export default ErrorLoadingWebPage
 const styles = StyleSheet.create({
     Container:{
         flex:1,
-        justifyContent:'center',
-        padding: 8
+        padding: 8,
+    },
+    ErrorImage:{
+        minHeight: 320,
+        borderRadius:18,
+        resizeMode:'cover',
     },
     bigText:{
         fontSize: 35,
         marginVertical:40,
         fontWeight:'800',
-        textAlign:'center'
+        textAlign:'center',
+        color:'#000000',
+        letterSpacing:1.5
     },
     smallText:{
-        fontSize:18,
-        marginVertical:40,
+        fontSize: 24,
+        marginVertical: 10,
         fontWeight:'700',
-        textAlign:'center'
+        textAlign:'center',
+    },
+    but:{
+        marginTop: 40,
+        backgroundColor:'#6f00ff',
+        paddingVertical: 12,
+        borderRadius: 10,
+        elevation: 5,
+    },
+    butText:{
+        fontSize:18,
+        textAlign:'center',
+        fontWeight:'700',
+        color:'white',
     }
 })
